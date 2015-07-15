bc2dbbb056e231279ec24154560c62262b62baeb
Tilo Werdin <werdin.tilo@arcor.de>
2015-07-15 02:32:39 +0200

    final version

--- TextFileIterator.java
+++ TextFileIterator.java
@@ -1,3 +1,5 @@
+import java.util.NoSuchElementException;
+
 public class TextFileIterator implements java.util.Iterator<String>
 {
     //private Resource res;
@@ -14,13 +16,16 @@ public class TextFileIterator implements java.util.Iterator<String>
             throw new IllegalArgumentException();
         }
 
-        restText = getAsString(res).toCharArray();
+        restText = getAsString(res).replace("-\n", "").toCharArray();
     }
 
     public boolean hasNext()
     {
+        if (restText.length == 0) {
+            return false;
+        }
         int i = 0;
-        while(Character.isDigit(restText[i]) == false && Character.isLetter(restText[i]) == false && i < restText.length){
+        while(i < restText.length && Character.isDigit(restText[i]) == false && Character.isLetter(restText[i]) == false){
             i++;
         }
         if (restText.length == i) {
@@ -31,27 +36,39 @@ public class TextFileIterator implements java.util.Iterator<String>
 
     public String next()
     {
+        if (restText.length == 0) {
+            throw new NoSuchElementException();
+        }
         String solution = "";
         int i = 0;
-        while(Character.isDigit(restText[i]) == false && Character.isLetter(restText[i]) == false && i < restText.length){
+        while((i < restText.length) && (Character.isDigit(restText[i]) == false) && (Character.isLetter(restText[i]) == false)){
             i++;
         }
 
-        while((Character.isDigit(restText[i]) || Character.isLetter(restText[i])) && i < restText.length)
+        if (i == restText.length) {
+            throw new NoSuchElementException();
+        }
+
+        while(i < restText.length && (Character.isDigit(restText[i]) || Character.isLetter(restText[i])))
         {
             solution += restText[i];
-            if (restText[i + 1] == 45 && restText[i + 2] == 92 && restText[i + 3] == 110) {
-                i += 3;
-            } 
+            //if ((i + 3 < restText.length) && ((restText[i + 1] == '-') && (restText[i + 2] == '\\' && (restText[i + 3] == 'n')))) {
+              //  i += 3;
+            //} 
             i++;
         }
 
         String text = "";
-        for (int j = i; j <= restText.length; j++) {
-            text += restText[j];
-        }
+        if (i == restText.length) {
+            restText = text.toCharArray();
+        } else {
 
-        restText = text.toCharArray();
+            for (int j = i; j < restText.length; j++) {
+                text += restText[j];
+            }
+
+            restText = text.toCharArray();
+        }
         return solution;
     }
 
